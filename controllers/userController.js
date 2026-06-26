a3148464cdccc042f754a816f85aeac180fb13da
sathsararns <sathsararns@gmail.com>
2026-06-26 21:37:35 +0530

    add getProfile endpoint to fetch authenticated user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -96,3 +96,23 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// GET PROFILE
+export const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.json(user);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
