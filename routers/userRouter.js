e265e24eea170d928418914bac0465176b550d2b
sathsararns <sathsararns@gmail.com>
2026-06-27 13:22:24 +0530

    Add login route to userRouter

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
   createUser,
-
+  loginUser,
+  
 } from "../controllers/userController.js";
 
 import authenticate from "../middlewares/authenticate.js";
@@ -9,6 +10,6 @@ import authenticate from "../middlewares/authenticate.js";
 const router = express.Router();
 
 router.post("/register", createUser);
-
+router.post("/login", loginUser);
 
 export default router;
