016c6e21ba080151fc19f3cef22cca465b60b53e
sathsararns <sathsararns@gmail.com>
2026-06-28 11:31:14 +0530

    add block and unblock user functionality in adminRouter

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -21,5 +21,26 @@ router.get("/bookings", authenticate, adminOnly, async (req, res) => {
   res.json(bookings);
 });
 
+// BLOCK USER
+router.put("/block/:id", authenticate, adminOnly, async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    { isBlocked: true },
+    { new: true }
+  );
+
+  res.json({ message: "User blocked", user });
+});
+
+// UNBLOCK USER
+router.put("/unblock/:id", authenticate, adminOnly, async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    { isBlocked: false },
+    { new: true }
+  );
+
+  res.json({ message: "User unblocked", user });
+});
 
 export default router;
