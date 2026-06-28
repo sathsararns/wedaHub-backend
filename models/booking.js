04b69195fef59339ac8f0c61cf335cca5a790e54
sathsararns <sathsararns@gmail.com>
2026-06-28 11:04:55 +0530

    add core booking model with customer, provider, service, and date

---
+++ models/booking.js
@@ -0,0 +1,33 @@
+// models/Booking.js
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    providerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    serviceName: {
+      type: String,
+      required: true,
+    },
+
+    description: String,
+
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Booking", bookingSchema);
