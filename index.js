f78bf0948898583811582c78e1d030e0c3b3e82e
sathsararns <sathsararns@gmail.com>
2026-07-06 21:14:26 +0530

    feat: enhance user login and socket connection logging for better traceability

--- index.js
+++ index.js
@@ -31,10 +31,16 @@ const io = new Server(server, {
 export { io };
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  console.log("Socket Connected:", socket.id);
+
+  socket.on("join", (userId) => {
+    socket.join(userId);
+
+    console.log(`User ${userId} joined room`);
+  });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("Socket disconnected:", socket.id);
   });
 });
 
